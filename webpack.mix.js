ac77406f9fab938d8ca3b50993677d8f022b0a7b
karterrr <tagaev.a@bk.ru>
2021-12-14 18:19:42 +1000

    Home work 2 Complete

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,8 +15,13 @@ mix
     .setPublicPath('public')
     .js('resources/scripts/main.js', 'build/bundle.js')
     .sass('resources/styles/main.scss', 'build/bundle.css')
-    //.browserSync({
-    //    proxy: 'localhost',
-    //    files: ['resources/**/*'],
-    //});
+    .browserSync({
+        proxy: 'localhost',
+        files: [
+            'public/**/*',
+            'resources/**/*.twig',
+            'resources/**/*.blade.php',
+            'resources/**/*.php'
+        ],
+    });
 
