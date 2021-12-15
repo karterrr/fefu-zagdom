e23dc90115721f11506f0f066f5c50338447599d
karterrr <tagaev.a@bk.ru>
2021-12-15 22:21:31 +1000

    HomeWork3
    Шапка Сайта

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const { resolve } = require('path');
 
 /*
  |--------------------------------------------------------------------------
@@ -14,14 +15,26 @@ const mix = require('laravel-mix');
 mix
     .setPublicPath('public')
     .js('resources/scripts/main.js', 'build/bundle.js')
-    .sass('resources/styles/main.scss', 'build/bundle.css')
+    .sass('resources/styles/main.scss', 'build/bundle.css', {}, [
+        require('postcss-axis')
+    ])
     .browserSync({
+        watch: true,
         proxy: 'localhost',
         files: [
+            'resources/components/footer/**/*',
+            'resources/components/header/**/*',
+            'resources/components/**/*',
+            'resources/lang/**/*',
+            'resources/layouts/**/*',
             'public/**/*',
-            'resources/**/*.twig',
-            'resources/**/*.blade.php',
-            'resources/**/*.php'
+            'resources/views/**/*',
+            'resources/scripts/**/*',
+            'resources/styles/**/*',
+            'resources/routes/**/*',
+            'resources/**/*',
         ],
     });
+
+mix.alias({'uikit-util': resolve(__dirname, 'node_modules/uikit/src/js/util')});
 
